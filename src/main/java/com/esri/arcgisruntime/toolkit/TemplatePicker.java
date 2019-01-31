01c266e10ca92bbac4bf6bc96f2bf1894d64bdd4
Colin Anderson <colin_anderson@esri.com>
2019-01-31 11:59:39 +0000

    Use layoutChildren, add doc

--- src/main/java/com/esri/arcgisruntime/toolkit/TemplatePicker.java
+++ src/main/java/com/esri/arcgisruntime/toolkit/TemplatePicker.java
@@ -1,17 +1,17 @@
-/*
- COPYRIGHT 1995-2019 ESRI
-
- TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
- Unpublished material - all rights reserved under the
- Copyright Laws of the United States.
-
- For additional information, contact:
- Environmental Systems Research Institute, Inc.
- Attn: Contracts Dept
- 380 New York Street
- Redlands, California, USA 92373
-
- email: contracts@esri.com
+/*/*
+ * Copyright 2019 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.esri.arcgisruntime.toolkit;
@@ -22,6 +22,7 @@ import java.util.Objects;
 import com.esri.arcgisruntime.data.FeatureTemplate;
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.toolkit.skins.TemplatePickerSkin;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -30,43 +31,121 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.Control;
 import javafx.scene.control.Skin;
 
-public class TemplatePicker extends Control {
+/**
+ * Template picker control which displays {@link FeatureTemplate}s that can be selected.
+ *
+ * @since 100.5
+ */
+public final class TemplatePicker extends Control {
 
-  ObservableList<FeatureLayer> featureLayers = FXCollections.observableList(new ArrayList<>());
-  SimpleListProperty<FeatureLayer> featureLayerListProperty = new SimpleListProperty<>(featureLayers);
-  SimpleObjectProperty<Template> selectedTemplateProperty = new SimpleObjectProperty<>();
-  SimpleIntegerProperty symbolSizeProperty = new SimpleIntegerProperty(50);
+  private final ObservableList<FeatureLayer> featureLayers = FXCollections.observableList(new ArrayList<>());
+  private final SimpleListProperty<FeatureLayer> featureLayerListProperty = new SimpleListProperty<>(featureLayers);
+  private final SimpleObjectProperty<Template> selectedTemplateProperty = new SimpleObjectProperty<>();
+  private final SimpleIntegerProperty symbolSizeProperty = new SimpleIntegerProperty(50);
+  private final SimpleBooleanProperty showTemplateNamesProperty = new SimpleBooleanProperty(false);
+  private final SimpleBooleanProperty showFeatureLayerNamesProperty = new SimpleBooleanProperty(true);
+  private final SimpleBooleanProperty showSeparatorsProperty = new SimpleBooleanProperty(true);
 
   @Override
   protected Skin<?> createDefaultSkin() {
     return new TemplatePickerSkin(this);
   }
 
+  /**
+   * Specifies the list of {@link FeatureLayer}s that the template picker will display feature templates
+   * for.
+   *
+   * @return the list property
+   * @since 100.5
+   */
   public SimpleListProperty<FeatureLayer> featureLayerListProperty() {
     return featureLayerListProperty;
   }
 
+  /**
+   * The selected template if any.
+   *
+   * @return the selected template property
+   * @since 100.5
+   */
   public SimpleObjectProperty<Template> selectedTemplateProperty() {
     return selectedTemplateProperty;
   }
 
+  /**
+   * Specifies the symbol size for the template's graphic.
+   * @return the property
+   * @since 100.5
+   */
   public SimpleIntegerProperty symbolSizeProperty() {
     return symbolSizeProperty;
   }
 
-  public static class Template {
+  /**
+   * Specifies if the names of the templates should be shown. The default value is false.
+   * @return the property
+   * @since 100.5
+   */
+  public SimpleBooleanProperty showTemplateNamesProperty() {
+    return showTemplateNamesProperty;
+  }
+
+  /**
+   * Specifies if feature laayer names should be shown. The default value is true.
+   * @return the property
+   * @since 100.5
+   */
+  public SimpleBooleanProperty showFeatureLayerNamesProperty() {
+    return showFeatureLayerNamesProperty;
+  }
+
+  /**
+   * Specifies if separators should be shown between feature layers. The default value is true.
+   * @return the property
+   * @since 100.5
+   */
+  public SimpleBooleanProperty showSeparatorsProperty() {
+    return showSeparatorsProperty;
+  }
+
+  /**
+   * A template which consists of a {@link FeatureTemplate} and the {@link FeatureLayer} to which the template is
+   * associated.
+   *
+   * @since 100.5
+   */
+  public final static class Template {
     private FeatureLayer featureLayer;
     private FeatureTemplate featureTemplate;
 
+    /**
+     * Creates a new instance.
+     *
+     * @param featureLayer the feature layer
+     * @param featureTemplate the feature template
+     * @since 100.5
+     * @throws NullPointerException if featureLayer is null
+     * @throws NullPointerException if featuretemplate is null
+     */
     public Template(FeatureLayer featureLayer, FeatureTemplate featureTemplate) {
       this.featureLayer = Objects.requireNonNull(featureLayer);
       this.featureTemplate = Objects.requireNonNull(featureTemplate);
     }
 
+    /**
+     * Gets the feature layer.
+     * @return the feature layer
+     * @since 100.5
+     */
     public FeatureLayer getFeatureLayer() {
       return featureLayer;
     }
 
+    /**
+     * Gets the feature template.
+     * @return the feature template
+     * @since 100.5
+     */
     public FeatureTemplate getFeatureTemplate() {
       return featureTemplate;
     }
--- src/main/java/com/esri/arcgisruntime/toolkit/skins/TemplatePickerSkin.java
+++ src/main/java/com/esri/arcgisruntime/toolkit/skins/TemplatePickerSkin.java
@@ -1,22 +1,24 @@
 /*
- COPYRIGHT 1995-2019 ESRI
-
- TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
- Unpublished material - all rights reserved under the
- Copyright Laws of the United States.
-
- For additional information, contact:
- Environmental Systems Research Institute, Inc.
- Attn: Contracts Dept
- 380 New York Street
- Redlands, California, USA 92373
-
- email: contracts@esri.com
+ * Copyright 2019 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.esri.arcgisruntime.toolkit.skins;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 
@@ -30,117 +32,193 @@ import com.esri.arcgisruntime.toolkit.TemplatePicker;
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.BooleanPropertyBase;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.css.PseudoClass;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
+import javafx.geometry.VPos;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Separator;
 import javafx.scene.control.SkinBase;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Region;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
+/**
+ * A skin for {@link TemplatePicker}.
+ *
+ * @since 100.5
+ */
 public class TemplatePickerSkin extends SkinBase<TemplatePicker> {
 
   private final SimpleListProperty<FeatureLayer> featureLayers = new SimpleListProperty<>();
   private final SimpleIntegerProperty symbolSizeProperty = new SimpleIntegerProperty();
-
-  private final ArrayList<TemplateCell> cells = new ArrayList<>();
+  private final SimpleBooleanProperty showTemplateNamesProperty = new SimpleBooleanProperty();
+  private final SimpleBooleanProperty showFeatureLayerNamesProperty = new SimpleBooleanProperty();
+  private final SimpleBooleanProperty showSeparatorsPropetry = new SimpleBooleanProperty();
 
   private final VBox vBox = new VBox();
   private final ScrollPane scrollPane = new ScrollPane(vBox);
 
-  public TemplatePickerSkin(TemplatePicker control) {
-    super(control);
+  private boolean invalid = true;
 
-    scrollPane.prefViewportWidthProperty().bind(vBox.widthProperty());
-    scrollPane.prefViewportHeightProperty().bind(vBox.heightProperty());
+  private final LinkedHashMap<FeatureLayer, List<TemplateCell>> cellMap = new LinkedHashMap<>();
+
+  /**
+   * Creates a new skin instance.
+   *
+   * @param control the control that is to be skinned
+   * @since 100.5
+   * @throws NullPointerException if control is null
+   */
+  public TemplatePickerSkin(TemplatePicker control) {
+    super(Objects.requireNonNull(control));
+
+    vBox.setPadding(new Insets(10.0));
+
     scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 
     getChildren().add(scrollPane);
 
     featureLayers.bind(control.featureLayerListProperty());
-    featureLayers.addListener((InvalidationListener) observable -> refresh());
+    featureLayers.addListener((InvalidationListener) observable -> populate());
 
     symbolSizeProperty.bind(control.symbolSizeProperty());
+    symbolSizeProperty.addListener(observable -> invalid = true);
 
-    refresh();
+    showTemplateNamesProperty.bind(control.showTemplateNamesProperty());
+    showTemplateNamesProperty.addListener(observable -> invalid = true);
+
+    showFeatureLayerNamesProperty.bind(control.showFeatureLayerNamesProperty());
+    showFeatureLayerNamesProperty.addListener(observable -> {
+      invalid = true;
+      control.requestLayout();
+    });
+
+    showSeparatorsPropetry.bind(control.showSeparatorsProperty());
+    showSeparatorsPropetry.addListener(observable -> {
+      invalid = true;
+      control.requestLayout();
+    });
+
+    populate();
   }
 
-  private void refresh() {
-    vBox.getChildren().clear();
-    cells.clear();
+  @Override
+  protected void layoutChildren(double contentX, double contentY, double contentWidth, double contentHeight) {
+    if (invalid) {
+      vBox.getChildren().clear();
+      cellMap.forEach(((featureLayer, templateCells) -> {
+        if (showFeatureLayerNamesProperty.get()) {
+          vBox.getChildren().add(new Label(featureLayer.getName()));
+        }
+        TilePane tilePane = new TilePane();
+        vBox.getChildren().add(tilePane);
+        tilePane.getChildren().addAll(templateCells);
+        if (showSeparatorsPropetry.get()) {
+          vBox.getChildren().add(new Separator());
+        }
+        invalid = false;
+      }));
+    }
+
+    getChildren().forEach(child -> layoutInArea(child, contentX, contentY, contentWidth, contentHeight, -1, HPos.CENTER, VPos.CENTER));
+  }
+
+  /**
+   * Updates the skin whenever the feature layer list is changed.
+   *
+   * @since 100.5
+   */
+  private void populate() {
+    cellMap.clear();
+
     getSkinnable().selectedTemplateProperty().set(null);
 
     featureLayers.stream().filter(entry -> entry.getFeatureTable() instanceof ArcGISFeatureTable)
       .forEach(featureLayer -> {
         switch (featureLayer.getLoadStatus()) {
           case NOT_LOADED:
-            // refresh list once the layer us loaded
-            featureLayer.addDoneLoadingListener(() -> refresh());
+            // populate list once the layer us loaded
+            featureLayer.addDoneLoadingListener(() -> populate());
             break;
           case FAILED_TO_LOAD:
             // do nothing - layer is ignored
             break;
           case LOADED:
+            ArrayList<TemplateCell> templateCells = new ArrayList<>();
+
             // layer is loaded so add cells for each template
             ArcGISFeatureTable featureTable = (ArcGISFeatureTable) featureLayer.getFeatureTable();
             Renderer renderer = featureLayer.getRenderer();
-            TilePane tilePane = new TilePane();
-            tilePane.setPrefColumns(3);
-            vBox.getChildren().add(tilePane);
 
             featureTable.getFeatureTemplates()
               .forEach(featureTemplate -> {
-                TemplateCell cell = createTemplateLabel(featureTemplate, featureLayer);
-                tilePane.getChildren().add(cell);
-                cells.add(cell);
+                TemplateCell cell = createTemplateCell(featureTemplate, featureLayer);
+                templateCells.add(cell);
               });
             featureTable.getFeatureTypes()
               .forEach(featureType -> featureType.getTemplates()
                 .forEach(featureTemplate -> {
-                  TemplateCell cell = createTemplateLabel(featureTemplate, featureLayer);
-                  tilePane.getChildren().add(cell);
-                  cells.add(cell);
+                  TemplateCell cell = createTemplateCell(featureTemplate, featureLayer);
+                  templateCells.add(cell);
                 }));
+            cellMap.put(featureLayer, templateCells);
+            break;
         }
       });
+    invalid = true;
+    getSkinnable().requestLayout();
   }
 
-  Region calculateRegion(Region region) {
-    Group root = new Group();
-    Scene dummyScene = new Scene(root);
-    root.getChildren().add(region);
-    root.applyCss();
-    root.layout();
-
-    return region;
-  }
-
-  private TemplateCell createTemplateLabel(FeatureTemplate featureTemplate, FeatureLayer featureLayer) {
+  /**
+   * Creates a new {@link TemplateCell} to display a template.
+   *
+   * @param featureTemplate the feature template
+   * @param featureLayer the feature layer
+   * @return a new cell
+   * @since 100.5
+   */
+  private TemplateCell createTemplateCell(FeatureTemplate featureTemplate, FeatureLayer featureLayer) {
     TemplateCell cell = new TemplateCell(new TemplatePicker.Template(featureLayer, featureTemplate), this);
 
     cell.setOnMouseClicked(e -> {
-      getSkinnable().selectedTemplateProperty().set(cell.getTemplate());
-      cells.forEach(c -> c.setSelected(false));
+      cellMap.forEach((layer, cells) -> cells.forEach(c -> c.setSelected(false)));
       cell.setSelected(true);
+      getSkinnable().selectedTemplateProperty().set(cell.getTemplate());
     });
 
     return cell;
   }
 
+  /**
+   * A control which displays a sample image for a feature template and optionally a template name. Also handles
+   * highlighting when the control is selected.
+   *
+   * @since 100.5
+   */
   static class TemplateCell extends Label {
     private final TemplatePicker.Template template;
     private final TemplatePickerSkin templatePickerSkin;
 
+    // define a psuedo calss that will highlight the control if it is selected
     private static final PseudoClass SELECTED_PSEUDO_CLASS = PseudoClass.getPseudoClass("selected");
     private BooleanProperty selectedProperty;
 
+    /**
+     * Creates a new instance.
+     *
+     * @param template the feature template
+     * @param templatePickerSkin the skin that this control will be associated with
+     * @since 100.5
+     * @throws NullPointerException if template is null
+     * @throws NullPointerException if templatePickerSkin is null
+     */
     TemplateCell(TemplatePicker.Template template, TemplatePickerSkin templatePickerSkin) {
       this.template = Objects.requireNonNull(template);
       this.templatePickerSkin = Objects.requireNonNull(templatePickerSkin);
@@ -151,15 +229,28 @@ public class TemplatePickerSkin extends SkinBase<TemplatePicker> {
 
       setMaxWidth(Double.MAX_VALUE);
       setPadding(new Insets(5.0));
+      setContentDisplay(ContentDisplay.LEFT);
 
       templatePickerSkin.symbolSizeProperty.addListener(observable -> update());
+      templatePickerSkin.showTemplateNamesProperty.addListener(observable -> update());
 
       update();
     }
 
+    /**
+     * Updates the control when the content or appearance changes.
+     *
+     * @since 100.5
+     */
     private void update() {
       FeatureTemplate featureTemplate = template.getFeatureTemplate();
       FeatureLayer featureLayer = template.getFeatureLayer();
+
+      if (templatePickerSkin.showTemplateNamesProperty.get()) {
+        setText(featureTemplate.getName());
+      } else {
+        setText(null);
+      }
 
       Graphic graphic = new Graphic();
       graphic.getAttributes().putAll(featureTemplate.getPrototypeAttributes());
@@ -173,18 +264,41 @@ public class TemplatePickerSkin extends SkinBase<TemplatePicker> {
       setTooltip(new Tooltip(featureLayer.getName() + " : " + featureTemplate.getName()));
     }
 
+    /**
+     * Gets the template assocaited with this control.
+     *
+     * @return the template
+     */
     public TemplatePicker.Template getTemplate() {
       return template;
     }
 
+    /**
+     * Sets the control to be selected or not.
+     *
+     * @param selected true to select, false otherwise
+     * @since 100.5
+     */
     public final void setSelected(boolean selected) {
       selectedProperty().set(selected);
     }
 
+    /**
+     * Gets if the control is selected or not.
+     *
+     * @return true if selected, otherwise false
+     * @since 100.5
+     */
     public final boolean isSelected() {
       return selectedProperty != null && selectedProperty.get();
     }
 
+    /**
+     * Gets the property representing if the control is selected or not.
+     *
+     * @return the property
+     * @since 100.5
+     */
     public final BooleanProperty selectedProperty() {
       if (selectedProperty == null) {
         selectedProperty = new BooleanPropertyBase(false) {
