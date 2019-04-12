0b5dc3292b350f15beaaa1f29696dff1035de1ed
Colin Anderson <colin_anderson@esri.com>
2019-04-12 14:02:36 +0100

    Add doc

--- src/main/java/com/esri/arcgisruntime/toolkit/SketchEditorTemplateHelper.java
+++ src/main/java/com/esri/arcgisruntime/toolkit/SketchEditorTemplateHelper.java
@@ -23,7 +23,6 @@ import com.esri.arcgisruntime.geometry.GeometryType;
 import com.esri.arcgisruntime.layers.FeatureLayer;
 import com.esri.arcgisruntime.mapping.view.Graphic;
 import com.esri.arcgisruntime.mapping.view.SketchCreationMode;
-import com.esri.arcgisruntime.mapping.view.SketchEditor;
 import com.esri.arcgisruntime.mapping.view.SketchStyle;
 import com.esri.arcgisruntime.symbology.FillSymbol;
 import com.esri.arcgisruntime.symbology.LineSymbol;
@@ -34,13 +33,26 @@ import com.esri.arcgisruntime.symbology.MultilayerPolylineSymbol;
 import com.esri.arcgisruntime.symbology.MultilayerSymbol;
 import com.esri.arcgisruntime.symbology.Symbol;
 
+/**
+ * A helper class to create a SketchStyle and a SketchCreationMode for a FeatureTemplate.
+ *
+ * @since 100.5.0
+ */
 public final class SketchEditorTemplateHelper {
 
-  public static SketchCreationMode getSketchCreationMode(FeatureTemplate template, FeatureLayer featureLayer) {
-    SketchEditor sketchEditor = new SketchEditor();
-
-    GeometryType geometryType = Objects.requireNonNull(featureLayer).getFeatureTable().getGeometryType();
-    FeatureTemplate.DrawingTool drawingTool = Objects.requireNonNull(template).getDrawingTool();
+  /**
+   * Creates a sketch creation mode that best matches the template.
+   *
+   * @param featureTemplate the feature template
+   * @param featureLayer the feature layer the template will be used with
+   * @return the sketch creation mode
+   * @throws NullPointerException if featureTemplate is null
+   * @throws NullPointerException if featureLayer is null
+   * @since 100.5.0
+   */
+  public static SketchCreationMode createsSketchCreationMode(FeatureTemplate featureTemplate, FeatureLayer featureLayer) {
+    GeometryType geometryType = Objects.requireNonNull(featureLayer, "featureLayer").getFeatureTable().getGeometryType();
+    FeatureTemplate.DrawingTool drawingTool = Objects.requireNonNull(featureTemplate, "featureTemplate").getDrawingTool();
 
     // creation mode
     SketchCreationMode creationMode;
@@ -82,40 +94,50 @@ public final class SketchEditorTemplateHelper {
     return creationMode;
   }
 
-  public static SketchStyle getSketchStyle(FeatureTemplate template, FeatureLayer featureLayer) {
+  /**
+   * Creates a sketch style suitable for the feature template and feature layer.
+   *
+   * @param featureTemplate the feature template
+   * @param featureLayer the feature layer the template will be used with
+   * @return the sketch style
+   * @throws NullPointerException if featureTemplate is null
+   * @throws NullPointerException if featureLayer is null
+   * @since 100.5.0
+   */
+  public static SketchStyle createSketchStyle(FeatureTemplate featureTemplate, FeatureLayer featureLayer) {
     Graphic graphic = new Graphic();
-    graphic.getAttributes().putAll(template.getPrototypeAttributes());
-    Symbol symbol = featureLayer.getRenderer().getSymbol(graphic);
+    graphic.getAttributes().putAll(Objects.requireNonNull(featureTemplate, "featureTemplate").getPrototypeAttributes());
+    Symbol symbol = Objects.requireNonNull(featureLayer, "featureLayer").getRenderer().getSymbol(graphic);
 
     SketchStyle sketchStyle = new SketchStyle();
 
     if (symbol instanceof MultilayerSymbol) {
       if (symbol instanceof MultilayerPointSymbol) {
-        sketchStyle.setVertexSymbol((MultilayerPointSymbol) symbol);
-        sketchStyle.setFeedbackVertexSymbol((MultilayerPointSymbol) sketchStyle.getVertexSymbol());
-        sketchStyle.setSelectedVertexSymbol((MultilayerPointSymbol) sketchStyle.getVertexSymbol());
+        sketchStyle.setMultilayerVertexSymbol((MultilayerPointSymbol) symbol);
+        sketchStyle.setMultilayerFeedbackVertexSymbol(sketchStyle.getMultilayerVertexSymbol());
+        sketchStyle.setMultilayerSelectedVertexSymbol(sketchStyle.getMultilayerVertexSymbol());
       }
       if (symbol instanceof MultilayerPolylineSymbol) {
-        sketchStyle.setLineSymbol((MultilayerPolylineSymbol) symbol);
-        sketchStyle.setFeedbackLineSymbol((MultilayerPolylineSymbol) sketchStyle.getLineSymbol());
+        sketchStyle.setMultilayerLineSymbol((MultilayerPolylineSymbol) symbol);
+        sketchStyle.setMultilayerFeedbackLineSymbol(sketchStyle.getMultilayerLineSymbol());
       }
       if (symbol instanceof MultilayerPolygonSymbol) {
-        sketchStyle.setFillSymbol((MultilayerPolygonSymbol) symbol);
-        sketchStyle.setFeedbackFillSymbol((MultilayerPolygonSymbol) sketchStyle.getFillSymbol());
+        sketchStyle.setMultilayerFillSymbol((MultilayerPolygonSymbol) symbol);
+        sketchStyle.setMultilayerFeedbackFillSymbol(sketchStyle.getMultilayerFillSymbol());
       }
     } else {
       if (symbol instanceof MarkerSymbol) {
         sketchStyle.setVertexSymbol((MarkerSymbol) symbol);
-        sketchStyle.setFeedbackVertexSymbol((MarkerSymbol) sketchStyle.getVertexSymbol());
-        sketchStyle.setSelectedVertexSymbol((MarkerSymbol) sketchStyle.getVertexSymbol());
+        sketchStyle.setFeedbackVertexSymbol(sketchStyle.getVertexSymbol());
+        sketchStyle.setSelectedVertexSymbol(sketchStyle.getVertexSymbol());
       }
       if (symbol instanceof LineSymbol) {
         sketchStyle.setLineSymbol((LineSymbol) symbol);
-        sketchStyle.setFeedbackLineSymbol((LineSymbol) sketchStyle.getLineSymbol());
+        sketchStyle.setFeedbackLineSymbol(sketchStyle.getLineSymbol());
       }
       if (symbol instanceof FillSymbol) {
         sketchStyle.setFillSymbol((FillSymbol) symbol);
-        sketchStyle.setFeedbackFillSymbol((FillSymbol) sketchStyle.getFillSymbol());
+        sketchStyle.setFeedbackFillSymbol(sketchStyle.getFillSymbol());
       }
     }
 
