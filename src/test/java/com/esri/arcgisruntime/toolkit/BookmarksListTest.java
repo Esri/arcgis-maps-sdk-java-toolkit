3285e8791a6b768fab20cb52b34cd6bde2aec331
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-10-28 13:23:01 -0700

    add unit test for case where map or scene in geoview is null

--- src/test/java/com/esri/arcgisruntime/toolkit/BookmarksListTest.java
+++ src/test/java/com/esri/arcgisruntime/toolkit/BookmarksListTest.java
@@ -1,18 +1,39 @@
 package com.esri.arcgisruntime.toolkit;
 
-import javafx.application.Platform;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.SceneView;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.testfx.framework.junit.ApplicationTest;
 
-public class BookmarksListTest {
+/**
+ * Unit tests for BookmarksList.
+ */
+public class BookmarksListTest extends ApplicationTest {
 
     /**
      * Tests IllegalArgumentException from null geoView constructor arg.
      */
     @Test
     public void constructorGeoViewNull() {
-        Platform.startup(() ->
-            Assertions.assertThrows(IllegalArgumentException.class, () -> new BookmarksList(null))
-        );
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new BookmarksList(null));
+    }
+
+    /**
+     * Tests IllegalStateException from null map in map view.
+     */
+    @Test
+    public void mapViewWithoutMap() {
+        MapView mapView = new MapView();
+        Assertions.assertThrows(IllegalStateException.class, () -> new BookmarksList(mapView));
+    }
+
+    /**
+     * Tests IllegalStateException from null scene in geo view.
+     */
+    @Test
+    public void sceneViewWithoutScene() {
+        SceneView sceneView = new SceneView();
+        Assertions.assertThrows(IllegalStateException.class, () -> new BookmarksList(sceneView));
     }
 }
