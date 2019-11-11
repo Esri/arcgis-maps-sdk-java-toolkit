d62124f31207b41a23b2169d9936a213f2a8f609
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-11-11 11:31:47 -0800

    rename to BookmarkListView

--- src/main/java/com/esri/arcgisruntime/toolkit/skins/BookmarksListSkin.java
+++ src/main/java/com/esri/arcgisruntime/toolkit/skins/BookmarksListSkin.java
@@ -3,14 +3,14 @@ package com.esri.arcgisruntime.toolkit.skins;
 import com.esri.arcgisruntime.mapping.Bookmark;
 import com.esri.arcgisruntime.mapping.view.ViewpointChangedEvent;
 import com.esri.arcgisruntime.mapping.view.ViewpointChangedListener;
-import com.esri.arcgisruntime.toolkit.BookmarkList;
+import com.esri.arcgisruntime.toolkit.BookmarkListView;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SkinBase;
 
 /**
  * A Skin to display the BookmarksWidget as a ListView.
  */
-public class BookmarksListSkin extends SkinBase<BookmarkList> {
+public class BookmarksListSkin extends SkinBase<BookmarkListView> {
 
   private final ListView<Bookmark> listView;
 
@@ -19,7 +19,7 @@ public class BookmarksListSkin extends SkinBase<BookmarkList> {
    *
    * @param control bookmarks list control to skin
    */
-  public BookmarksListSkin(BookmarkList control) {
+  public BookmarksListSkin(BookmarkListView control) {
     super(control);
 
     // show the control as a list view
@@ -27,16 +27,15 @@ public class BookmarksListSkin extends SkinBase<BookmarkList> {
     getChildren().add(listView);
 
     // bind the items from the control
-    listView.itemsProperty().bind(control.bookmarksProperty());
+    listView.itemsProperty().set(control.getBookmarks());
 
     // default to showing the bookmark's name
     listView.cellFactoryProperty().bind(control.cellFactoryProperty());
 
     // set a listener for when the selected item changes
     listView.getSelectionModel().selectedItemProperty().addListener(listener -> {
-      if (control.getDeselectOnViewpointChange()) {
         Bookmark selectedBookmark = listView.getSelectionModel().getSelectedItem();
-        if (selectedBookmark != null) {
+        if (selectedBookmark != null && control.getGeoView() != null) {
           // default to deselecting the bookmark after changing viewpoint so it can be selected again
           control.getGeoView().setBookmarkAsync(selectedBookmark).addDoneListener(() ->
               control.getGeoView().addViewpointChangedListener(new ViewpointChangedListener() {
@@ -51,7 +50,6 @@ public class BookmarksListSkin extends SkinBase<BookmarkList> {
               })
           );
         }
-      }
     });
   }
 
