e577de388f1dad4a2f2fad998c05944a443cfab2
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-11-11 11:28:45 -0800

    change name to BookmarkListView

--- src/main/java/com/esri/arcgisruntime/toolkit/BookmarkListView.java
+++ src/main/java/com/esri/arcgisruntime/toolkit/BookmarkListView.java
@@ -3,30 +3,44 @@ package com.esri.arcgisruntime.toolkit;
 import com.esri.arcgisruntime.mapping.Bookmark;
 import com.esri.arcgisruntime.mapping.view.GeoView;
 import com.esri.arcgisruntime.toolkit.skins.BookmarksListSkin;
-import javafx.beans.property.BooleanProperty;
+import javafx.beans.NamedArg;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Skin;
 import javafx.util.Callback;
 
-public class BookmarkList extends BookmarkView {
+import java.util.List;
+
+public class BookmarkListView extends BookmarkView {
 
   private final ObjectProperty<Callback<ListView<Bookmark>, ListCell<Bookmark>>> cellFactory;
 
-  private final BooleanProperty deselectOnViewpointChange;
-
   /**
-   * Creates an instance for the GeoView.
+   * Creates an instance in which the bookmarks are bound to a GeoView's map or scene.
    *
    * @param geoView A GeoView
    */
-  public BookmarkList(GeoView geoView) {
+  public BookmarkListView(@NamedArg("geoView") GeoView geoView) {
     super(geoView);
     cellFactory = new SimpleObjectProperty<>(param -> new BookmarkListCell());
-    deselectOnViewpointChange = new SimpleBooleanProperty(true);
+  }
+
+  public BookmarkListView(@NamedArg("bookmarks") ObservableList<Bookmark> bookmarks) {
+    super(bookmarks);
+    cellFactory = new SimpleObjectProperty<>(param -> new BookmarkListCell());
+  }
+
+  public BookmarkListView(@NamedArg("bookmarks") List<Bookmark> bookmarks) {
+    super(bookmarks);
+    cellFactory = new SimpleObjectProperty<>(param -> new BookmarkListCell());
+  }
+
+  public BookmarkListView() {
+    super();
+    cellFactory = new SimpleObjectProperty<>(param -> new BookmarkListCell());
   }
 
   @Override
@@ -59,18 +73,6 @@ public class BookmarkList extends BookmarkView {
    */
   public void setCellFactory(Callback<ListView<Bookmark>, ListCell<Bookmark>> cellFactory) {
     this.cellFactory.set(cellFactory);
-  }
-
-  public boolean getDeselectOnViewpointChange() {
-    return deselectOnViewpointChange.get();
-  }
-
-  public BooleanProperty deselectOnViewpointChangeProperty() {
-    return deselectOnViewpointChange;
-  }
-
-  public void setDeselectOnViewpointChange(boolean deselectOnViewpointChange) {
-    this.deselectOnViewpointChange.set(deselectOnViewpointChange);
   }
 
   public static class BookmarkListCell extends ListCell<Bookmark> {
