e0b886b912215f2a95585d4e61eb9f880f360f72
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-11-14 14:09:20 -0800

    move cases into single logical test

--- src/test/java/com/esri/arcgisruntime/toolkit/utils/ListenableListUtilsTest.java
+++ src/test/java/com/esri/arcgisruntime/toolkit/utils/ListenableListUtilsTest.java
@@ -1,91 +1,69 @@
 package com.esri.arcgisruntime.toolkit.utils;
 
-import com.esri.arcgisruntime.mapping.*;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Bookmark;
+import com.esri.arcgisruntime.mapping.Viewpoint;
 import com.esri.arcgisruntime.util.ListenableList;
 import javafx.collections.ObservableList;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * Unit tests for ListenableListUtils.
+ */
 public class ListenableListUtilsTest {
 
   private final Bookmark guitarShapedTreesBookmark = new Bookmark("Guitar-shaped trees", new Viewpoint(-33.867886,
       -63.985, 4e4));
   private final Bookmark grandPrismaticSpringBookmark = new Bookmark("Grand Prismatic Spring", new Viewpoint(44.525049,
       -110.83819, 6e3));
+  private final Bookmark strangeSymbolBookmark = new Bookmark("Strange Symbol", new Viewpoint(37.401573, -116.867808,
+      6e3));
 
+  /**
+   * Tests the initializing, adding, and removing items from the observable list created with toObservableList.
+   */
   @Test
-  public void addItemsToListenableList() {
-    // given an observable list from a listenable list
-    ArcGISMap map = new ArcGISMap();
-    ListenableList<Bookmark> listenableList = map.getBookmarks();
-    ObservableList<Bookmark> observableList = ListenableListUtils.toObservableList(listenableList);
-
-    // when items are added to the listenable list
-    listenableList.addAll(Arrays.asList(guitarShapedTreesBookmark, grandPrismaticSpringBookmark));
-
-    // the observable list is updated with the items
-    Assertions.assertTrue(observableList.containsAll(listenableList));
-  }
-
-  @Test
-  public void initItemsFromListenableList() {
+  public void toObservableList() {
     // given a listenable list with some items
     ArcGISMap map = new ArcGISMap();
     ListenableList<Bookmark> listenableList = map.getBookmarks();
-    listenableList.addAll(Arrays.asList(guitarShapedTreesBookmark, grandPrismaticSpringBookmark));
+    listenableList.add(guitarShapedTreesBookmark);
 
-    // when the observable list is created
+    // when the observable list is created from the listenable list
     ObservableList<Bookmark> observableList = ListenableListUtils.toObservableList(listenableList);
 
-    // the observable list is initialized with the items
+    // it will also contain the items
+    Assertions.assertTrue(observableList.contains(guitarShapedTreesBookmark));
+
+    // when items are added to the listenable list
+    List<Bookmark> bookmarks = new ArrayList<>();
+    bookmarks.add(grandPrismaticSpringBookmark);
+    bookmarks.add(strangeSymbolBookmark);
+    listenableList.addAll(bookmarks);
+
+    // the observable list is updated with the items
     Assertions.assertTrue(observableList.containsAll(listenableList));
-  }
 
-  @Test
-  public void removeItemFromListenableList() {
-    // given an observable list from a listenable list with some items
-    ArcGISMap map = new ArcGISMap();
-    ListenableList<Bookmark> listenableList = map.getBookmarks();
-    listenableList.addAll(Arrays.asList(guitarShapedTreesBookmark, grandPrismaticSpringBookmark));
-    ObservableList<Bookmark> observableList = ListenableListUtils.toObservableList(listenableList);
-
-    // when an item is removed
+    // when an item is removed from the listenable list
     listenableList.remove(guitarShapedTreesBookmark);
 
-    // the observable list also shows the item removed
+    // it is removed from the observable list
     Assertions.assertFalse(observableList.contains(guitarShapedTreesBookmark));
-  }
 
-  @Test
-  public void addItemToObservableList() {
-    // given an observable list from a listenable list
-    ArcGISMap map = new ArcGISMap();
-    ListenableList<Bookmark> listenableList = map.getBookmarks();
-    ObservableList<Bookmark> observableList = ListenableListUtils.toObservableList(listenableList);
+    // when an item is added to the observable list
+    observableList.add(guitarShapedTreesBookmark);
 
-    // when items are added to the observable list
-    observableList.addAll(guitarShapedTreesBookmark, grandPrismaticSpringBookmark);
+    // it will be reflected to the listenable list
+    Assertions.assertTrue(listenableList.contains(guitarShapedTreesBookmark));
 
-    // the listenable also shows the items
-    Assertions.assertTrue(listenableList.containsAll(Arrays.asList(guitarShapedTreesBookmark,
-        grandPrismaticSpringBookmark)));
-  }
+    // when the item is removed from the observable list
+    observableList.remove(grandPrismaticSpringBookmark);
 
-  @Test
-  public void removeItemsFromObservableList() {
-    // given an observable list from a listenable list with some items
-    ArcGISMap map = new ArcGISMap();
-    ListenableList<Bookmark> listenableList = map.getBookmarks();
-    listenableList.addAll(Arrays.asList(guitarShapedTreesBookmark, grandPrismaticSpringBookmark));
-    ObservableList<Bookmark> observableList = ListenableListUtils.toObservableList(listenableList);
-    Assertions.assertEquals(2, observableList.size());
-
-    // when items are added to the observable list
-    observableList.removeAll(guitarShapedTreesBookmark, grandPrismaticSpringBookmark);
-
-    // the listenable also shows the items
-    Assertions.assertEquals(0, listenableList.size());
+    // the item is also removed from the listenable list
+    Assertions.assertFalse(listenableList.contains(grandPrismaticSpringBookmark));
   }
 }
