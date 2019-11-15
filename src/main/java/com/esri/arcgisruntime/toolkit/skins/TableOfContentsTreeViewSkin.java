e6bfb60373d3bc097724125a31020484c8bf7198
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-11-15 14:16:28 -0800

    handle when layers are added/removed, fix async name loading

--- src/main/java/com/esri/arcgisruntime/toolkit/skins/TableOfContentsTreeViewSkin.java
+++ src/main/java/com/esri/arcgisruntime/toolkit/skins/TableOfContentsTreeViewSkin.java
@@ -2,19 +2,12 @@ package com.esri.arcgisruntime.toolkit.skins;
 
 import com.esri.arcgisruntime.layers.Layer;
 import com.esri.arcgisruntime.layers.LayerContent;
-import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.toolkit.TableOfContents;
-import com.esri.arcgisruntime.toolkit.utils.ObservableListUtils;
-import javafx.beans.InvalidationListener;
 import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
-import javafx.scene.Node;
-import javafx.scene.control.*;
+import javafx.scene.control.SkinBase;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 /**
@@ -35,83 +28,79 @@ public class TableOfContentsTreeViewSkin extends SkinBase<TableOfContents> {
     getChildren().add(treeView);
 
     // set a cell factory which displays the layer content's name
-    treeView.setCellFactory(param -> new LayerContentTreeCell());
+    treeView.setCellFactory(new TableOfContents.LayerContentTreeCellFactory());
 
     // create an empty root node which will stay hidden
     TreeItem<LayerContent> root = new TreeItem<>();
     treeView.setRoot(root);
     treeView.setShowRoot(false);
 
-    ObservableList<Layer> aggregatedList = ObservableListUtils.createAggregatedObservableList(control.getReferenceLayers(),
-        control.getOperationalLayers(), control.getBaseLayers());
+    // set up operational layers
+    control.getOperationalLayers().forEach(layer -> {
+      TreeItem<LayerContent> item = new TableOfContents.LayerContentTreeItem(layer);
+      root.getChildren().add(item);
+      layer.addLoadStatusChangedListener(loadStatusChangedEvent -> {
+        // replace the item in the tree view whenever it's load status changes to display the status accordingly
+        layer.addDoneLoadingListener(() ->
+            root.getChildren().replaceAll(layerContentTreeItem ->
+                layerContentTreeItem.equals(item) ? item : layerContentTreeItem
+            )
+        );
+      });
+    });
 
-    // add all of the layers from the control as child nodes
-    aggregatedList.forEach(layer -> root.getChildren().add(new LayerContentTreeItem(layer)));
-
-    aggregatedList.addListener((ListChangeListener<Layer>) c -> {
+    control.getOperationalLayers().addListener((ListChangeListener<Layer>) c -> {
       while (c.next()) {
+        int insertionIndex = control.getReferenceLayers().size();
         if (c.wasAdded()) {
-          root.getChildren().addAll(c.getFrom(),
-              c.getAddedSubList().stream().map(LayerContentTreeItem::new).collect(Collectors.toList()));
+          root.getChildren().addAll(insertionIndex + c.getFrom(),
+              c.getAddedSubList().stream().map(TableOfContents.LayerContentTreeItem::new).collect(Collectors.toList()));
         } else if (c.wasRemoved()) {
-          root.getChildren().remove(c.getFrom(), c.getTo());
+          root.getChildren().remove(insertionIndex, insertionIndex + c.getRemovedSize());
         }
       }
     });
 
-  }
+    control.operationalLayersProperty().addListener((observable, oldValue, newValue) -> {
+      int insertionIndex = control.getReferenceLayers().size();
+      root.getChildren().remove(insertionIndex, insertionIndex + oldValue.size());
+      root.getChildren().addAll(insertionIndex,
+          newValue.stream().map(TableOfContents.LayerContentTreeItem::new).collect(Collectors.toList()));
+    });
 
-  public static class LayerContentTreeCell extends TreeCell<LayerContent> {
-    @Override
-    protected void updateItem(LayerContent item, boolean empty) {
-      super.updateItem(item, empty);
-      if (empty || item == null) {
-        setText(null);
-        setGraphic(null);
-      } else {
-        CheckBox visibilityToggleCheckBox = new CheckBox();
-        visibilityToggleCheckBox.setSelected(item.isVisible());
-        visibilityToggleCheckBox.selectedProperty().addListener(o ->
-            item.setVisible(visibilityToggleCheckBox.isSelected())
+    // set up base layers to show last
+    control.getBaseLayers().forEach(layer -> {
+      TreeItem<LayerContent> item = new TableOfContents.LayerContentTreeItem(layer);
+      root.getChildren().add(item);
+      layer.addLoadStatusChangedListener(loadStatusChangedEvent -> {
+        // replace the item in the tree view whenever it's load status changes to display the status accordingly
+        layer.addDoneLoadingListener(() ->
+          root.getChildren().replaceAll(layerContentTreeItem ->
+              layerContentTreeItem.equals(item) ? item : layerContentTreeItem
+          )
         );
+      });
+    });
 
-        setText(item.getName());
-        setGraphic(item.canChangeVisibility() ? visibilityToggleCheckBox : null);
-
-        // layer may need to be loaded before name is populated
-        if (item instanceof Layer) {
-          Layer layer = (Layer) item;
-          layer.loadAsync();
-          layer.addDoneLoadingListener(() -> setText(layer.getName()));
+    control.getBaseLayers().addListener((ListChangeListener<Layer>) c -> {
+      while (c.next()) {
+        int insertionIndex = control.getOperationalLayers().size() + control.getReferenceLayers().size();
+        if (c.wasAdded()) {
+          root.getChildren().addAll(insertionIndex + c.getFrom(),
+              c.getAddedSubList().stream().map(TableOfContents.LayerContentTreeItem::new).collect(Collectors.toList()));
+        } else if (c.wasRemoved()) {
+          root.getChildren().remove(insertionIndex + c.getFrom(), insertionIndex + c.getTo());
         }
       }
-    }
-  }
+    });
 
+    control.baseLayersProperty().addListener((observable, oldValue, newValue) -> {
+      int insertionIndex = control.getOperationalLayers().size() + control.getReferenceLayers().size();
+      root.getChildren().remove(insertionIndex, insertionIndex + oldValue.size());
+      root.getChildren().addAll(insertionIndex,
+          newValue.stream().map(TableOfContents.LayerContentTreeItem::new).collect(Collectors.toList()));
+    });
 
-  public static class LayerContentTreeItem extends TreeItem<LayerContent> {
-
-    public LayerContentTreeItem(LayerContent value, Node graphic) {
-      super(value, graphic);
-      value.getSubLayerContents().forEach(layerContent ->
-          getChildren().add(new LayerContentTreeItem(layerContent))
-      );
-    }
-
-    public LayerContentTreeItem(LayerContent value) {
-      super(value);
-      if (value instanceof Layer) {
-        Layer layer = (Layer) value;
-        layer.loadAsync();
-        layer.addDoneLoadingListener(() -> {
-          if (layer.getLoadStatus() == LoadStatus.LOADED) {
-            layer.getSubLayerContents().forEach(layerContent ->
-                getChildren().add(new LayerContentTreeItem(layerContent))
-            );
-          }
-        });
-      }
-    }
   }
 
 }
