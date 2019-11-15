6a57bc58f72c184a8356f4d663a9011766cb4ca4
Tyler Schiewe <tyler.schiewe@gmail.com>
2019-11-15 14:15:15 -0800

    cherry pick toObservableList updates

--- src/main/java/com/esri/arcgisruntime/toolkit/utils/ListenableListUtils.java
+++ src/main/java/com/esri/arcgisruntime/toolkit/utils/ListenableListUtils.java
@@ -2,7 +2,10 @@ package com.esri.arcgisruntime.toolkit.utils;
 
 import com.esri.arcgisruntime.util.ListenableList;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
 
 /**
  * Utility methods for ListenableLists.
@@ -10,24 +13,35 @@ import javafx.collections.ObservableList;
 public class ListenableListUtils {
 
   /**
-   * Returns an ObservableList which mirrors the given ListenableList. ListChangedEvents are forwarded to the
-   * ObservableList's ListChangedListener.
+   * Returns an ObservableList which is backed by the given ListenableList. Operations on either list are reflected
+   * in the other.
    *
    * @param listenableList original listenable list
    * @param <T> list content type
    *
-   * @return an observable list that mirrors the listenable list
+   * @return an observable list
    */
   public static <T> ObservableList<T> toObservableList(ListenableList<T> listenableList) {
     ObservableList<T> observableList = FXCollections.observableArrayList(listenableList);
+    // forward listenable list changes to observable list
     listenableList.addListChangedListener(listChangedEvent -> {
       switch (listChangedEvent.getAction()) {
         case ADDED:
-          observableList.add(listChangedEvent.getIndex(), listenableList.get(listChangedEvent.getIndex()));
+          observableList.addAll(listChangedEvent.getIndex(), listChangedEvent.getItems());
           break;
         case REMOVED:
-          observableList.remove(listChangedEvent.getIndex());
+          observableList.removeAll(listChangedEvent.getItems());
           break;
+      }
+    });
+    // forward observable list changes to listenable list
+    observableList.addListener((ListChangeListener<T>) c -> {
+      while (c.next()) {
+        if (c.wasAdded() && !listenableList.containsAll(c.getAddedSubList())) {
+          listenableList.addAll(c.getFrom(), c.getAddedSubList());
+        } else if (c.wasRemoved() && !c.getList().containsAll(listenableList)) {
+          listenableList.removeAll(new ArrayList<>(listenableList.subList(c.getFrom(), c.getRemovedSize())));
+        }
       }
     });
     return observableList;
