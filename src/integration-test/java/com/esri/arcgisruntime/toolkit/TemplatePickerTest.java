40804ed6677173890853e24b8be075d4de4f09d9
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-02-06 14:39:19 -0800

    update tests

--- src/integration-test/java/com/esri/arcgisruntime/toolkit/TemplatePickerTest.java
+++ src/integration-test/java/com/esri/arcgisruntime/toolkit/TemplatePickerTest.java
@@ -1,21 +1,21 @@
 package com.esri.arcgisruntime.toolkit;
 
-import com.esri.arcgisruntime.data.FeatureTable;
+import com.esri.arcgisruntime.data.ArcGISFeatureTable;
 import com.esri.arcgisruntime.data.ServiceFeatureTable;
 import com.esri.arcgisruntime.layers.FeatureLayer;
+import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
-import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import javafx.application.Platform;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Separator;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.junit.After;
 import org.junit.Test;
-import org.testfx.api.FxRobotException;
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
 import org.testfx.util.WaitForAsyncUtils;
@@ -26,34 +26,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TemplatePickerTest extends ApplicationTest {
 
-    private TemplatePicker templatePicker;
-    private FeatureLayer featureLayer;
-    private Scene scene;
+    final String WILDFIRE_RESPONSE_URL = "https://sampleserver6.arcgisonline" +
+        ".com/arcgis/rest/services/Wildfire/FeatureServer/0";
+
+    private StackPane stackPane;
 
     @Override
     public void start(Stage primaryStage) {
-        final String WILDFIRE_RESPONSE_URL = "https://sampleserver6.arcgisonline" +
-                ".com/arcgis/rest/services/Wildfire/FeatureServer/0";
-        FeatureTable featureTable = new ServiceFeatureTable(WILDFIRE_RESPONSE_URL);
-        featureLayer = new FeatureLayer(featureTable);
+        stackPane = new StackPane();
 
-        ArcGISMap map = new ArcGISMap(Basemap.createImagery());
-        map.getOperationalLayers().add(featureLayer);
-        MapView mapView = new MapView();
-        mapView.setPrefSize(500, 500);
-        mapView.setMap(map);
-
-        templatePicker = new TemplatePicker();
-        templatePicker.featureLayerListProperty().add(featureLayer);
-
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(mapView);
-        borderPane.setLeft(templatePicker);
-
-        scene = new Scene(borderPane);
+        Scene scene = new Scene(stackPane);
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.toFront();
+    }
+
+    @Test
+    public void templatePicker() {
+        ArcGISFeatureTable featureTable = new ServiceFeatureTable(WILDFIRE_RESPONSE_URL);
+        FeatureLayer featureLayer = new FeatureLayer(featureTable);
+
+        Platform.runLater(() -> {
+            FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayer);
+            stackPane.getChildren().add(featureTemplatePicker);
+        });
+
+        sleep(10000);
+
+        FeatureTemplatePicker featureTemplatePicker = (FeatureTemplatePicker) stackPane.getChildren().get(0);
+        featureTemplatePicker.getFeatureTemplateGroups().forEach(featureTemplateGroup ->
+            featureTemplateGroup.getFeatureTemplateItems().forEach(featureTemplateItem ->
+                clickOn(featureTemplateItem.getFeatureTemplate().getName())
+            )
+        );
+        clickOn(featureLayer.getName());
     }
 
     @After
@@ -61,104 +67,159 @@ public class TemplatePickerTest extends ApplicationTest {
         FxToolkit.cleanupStages();
     }
 
-    /**
-     * Tests that the title rendered for a template is updated when the layer's name changes.
-     */
     @Test
-    public void templateTitleUpdatesWhenLayerNameChanges() {
-        sleep(5000);
+    public void scrollable() {
+        ArcGISFeatureTable featureTable = new ServiceFeatureTable(WILDFIRE_RESPONSE_URL);
+        FeatureLayer featureLayer = new FeatureLayer(featureTable);
 
-        // given a template picker rendering a feature layer's name as the template's title
-        clickOn(featureLayer.getName()); // should not fail since name will be displayed and can be clicked
+        Platform.runLater(() -> {
+            FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayer, featureLayer);
+            featureTemplatePicker.setMaxSize(300, 300);
+            stackPane.getChildren().add(featureTemplatePicker);
+        });
 
-        // when the feature layer title is changed
-        featureLayer.setName("Test");
+        sleep(10000);
 
-        // then the title in the template picker will be updated
-        clickOn("Test");
+        Object[] scrollBars = lookup(n -> n instanceof ScrollBar).queryAll().toArray();
+        assertEquals(2, scrollBars.length);
+        assertTrue(((ScrollBar) scrollBars[0]).isVisible());
+        assertFalse(((ScrollBar) scrollBars[1]).isVisible());
+
+        FeatureTemplatePicker featureTemplatePicker = (FeatureTemplatePicker) stackPane.getChildren().get(0);
+        featureTemplatePicker.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+        WaitForAsyncUtils.waitForFxEvents();
+        sleep(4000);
+
+        assertFalse(((ScrollBar) scrollBars[0]).isVisible());
+        assertFalse(((ScrollBar) scrollBars[1]).isVisible());
     }
 
-    /**
-     * Tests that clicking on a selected cell does not trigger a selection changed event.
-     */
     @Test
-    public void subsequentClicksDoNotTriggerSelectionChanges() {
-        sleep(3000);
+    public void orientation() {
+        ArcGISFeatureTable featureTable = new ServiceFeatureTable(WILDFIRE_RESPONSE_URL);
+        FeatureLayer featureLayer = new FeatureLayer(featureTable);
 
-        // given a template which is selected
-        Set<TilePane> tilePanes = lookup(n -> n instanceof TilePane).queryAll();
-        assertEquals(1, tilePanes.size());
-        TilePane tilePane = tilePanes.iterator().next();
-        clickOn(tilePane.getChildren().get(0));
+        Platform.runLater(() -> {
+            FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayer, featureLayer);
+            featureTemplatePicker.setMaxSize(300, 300);
+            stackPane.getChildren().add(featureTemplatePicker);
+        });
 
-        // when the selected template is clicked again
-        templatePicker.selectedTemplateProperty().addListener(o -> fail("Should not be triggered"));
-        clickOn(tilePane.getChildren().get(0));
+        sleep(10000);
 
-        // then the selection property changed listener should not be fired again
-        sleep(1000);
-    }
+        Object[] scrollBars = lookup(n -> n instanceof ScrollBar).queryAll().toArray();
+        assertEquals(2, scrollBars.length);
 
-    /**
-     * Tests that clicking on a cell selects it.
-     */
-    @Test
-    public void clickingOnCellSelectsIt() {
-        sleep(3000);
+        assertTrue(((ScrollBar) scrollBars[0]).isVisible());
+        assertFalse(((ScrollBar) scrollBars[1]).isVisible());
 
-        // given a template which is selected programmatically
-        Set<TilePane> tilePanes = lookup(n -> n instanceof TilePane).queryAll();
-        assertEquals(1, tilePanes.size());
-        TilePane tilePane = tilePanes.iterator().next();
-
-        // when the selected template is clicked
-        clickOn(tilePane.getChildren().get(0));
-
-        // then the selection property changed listener should not be fired again
-        sleep(1000);
-
-        assertNotNull(templatePicker.selectedTemplateProperty().get());
-        assertEquals(featureLayer.getName(), templatePicker.selectedTemplateProperty().get().getFeatureLayer().getName());
-    }
-
-    /**
-     * Tests that separators between templates can be shown and hidden.
-     */
-    @Test
-    public void separatorsVisibility() {
-        sleep(2000);
-
-        // given a template picker with separators showing
-        templatePicker.showSeparatorsProperty().set(true);
-        clickOn(n -> n instanceof Separator); // should not fail
-
-        // when showing separators is set to false
-        templatePicker.showSeparatorsProperty().set(false);
-
-        sleep(1000);
-
-        // then no separators will be shown
-        assertThrows(FxRobotException.class, () -> clickOn(n -> n instanceof Separator), "No separators should be " +
-                "clickable.");
-    }
-
-    /**
-     * Tests that template names can be hidden.
-     */
-    @Test
-    public void templateNamesVisibility() {
-        sleep(3000);
-
-        // given a template picker with template names showing
-        clickOn(featureLayer.getName()); // should not fail
-
-        // when showing template names is set to false
-        Platform.runLater(() -> templatePicker.showTemplateNamesProperty().set(false));
+        FeatureTemplatePicker featureTemplatePicker = (FeatureTemplatePicker) stackPane.getChildren().get(0);
+        featureTemplatePicker.setOrientation(Orientation.HORIZONTAL);
 
         WaitForAsyncUtils.waitForFxEvents();
 
-        // then no name is shown
-        assertThrows(FxRobotException.class, () -> clickOn(featureLayer.getName()), "Name should not be clickable.");
+        assertFalse(((ScrollBar) scrollBars[0]).isVisible());
+        assertTrue(((ScrollBar) scrollBars[1]).isVisible());
+
+        featureTemplatePicker.setOrientation(Orientation.VERTICAL);
+
+        WaitForAsyncUtils.waitForFxEvents();
+
+        assertTrue(((ScrollBar) scrollBars[0]).isVisible());
+        assertFalse(((ScrollBar) scrollBars[1]).isVisible());
+    }
+
+    @Test
+    public void fromMap() {
+        MapView mapView = new MapView();
+        ArcGISMap map = new ArcGISMap("https://runtime.maps.arcgis.com/home/webmap/viewer.html?webmap=05792de90e1d4eff81fdbde8c5eb4063");
+        mapView.setMap(map);
+
+        Platform.runLater(() -> {
+            FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker();
+            featureTemplatePicker.setMaxWidth(500);
+            stackPane.getChildren().add(featureTemplatePicker);
+
+            map.addDoneLoadingListener(() -> {
+                if (map.getLoadStatus() == LoadStatus.LOADED) {
+                    map.getOperationalLayers().forEach(layer -> {
+                        if (layer instanceof FeatureLayer) {
+                            featureTemplatePicker.getFeatureLayers().add((FeatureLayer) layer);
+                        }
+                    });
+                }
+            });
+        });
+
+        sleep(5000);
+
+        FeatureTemplatePicker featureTemplatePicker = (FeatureTemplatePicker) stackPane.getChildren().get(0);
+        assertEquals(4, featureTemplatePicker.getFeatureTemplateGroups().size());
+
+        map.getOperationalLayers().forEach(layer -> clickOn(layer.getName()));
+    }
+
+    /**
+     * Tests wiring between items' toggle group and the selected feature template item property.
+     */
+    @Test
+    public void focusAndSelection() {
+        ArcGISFeatureTable featureTable = new ServiceFeatureTable(WILDFIRE_RESPONSE_URL);
+        FeatureLayer featureLayer = new FeatureLayer(featureTable);
+
+        Platform.runLater(() -> {
+            FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayer);
+            stackPane.getChildren().add(featureTemplatePicker);
+        });
+
+        sleep(3000);
+
+        FeatureTemplatePicker featureTemplatePicker = (FeatureTemplatePicker) stackPane.getChildren().get(0);
+
+        // given a template which is selected programmatically
+        Object[] toggleButtons = lookup(n -> n instanceof ToggleButton).queryAll().toArray();
+        assertTrue(toggleButtons.length > 1);
+        ToggleButton firstButton = (ToggleButton) toggleButtons[0];
+        ToggleButton secondButton = (ToggleButton) toggleButtons[1];
+
+        // when the selected template is clicked
+        clickOn(firstButton);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertEquals(firstButton.getUserData(), featureTemplatePicker.getSelectedFeatureTemplateItem());
+
+        clickOn(firstButton);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNull(featureTemplatePicker.getSelectedFeatureTemplateItem());
+
+        clickOn(firstButton);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertEquals(firstButton.getUserData(), featureTemplatePicker.getSelectedFeatureTemplateItem());
+
+        clickOn(secondButton);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertEquals(secondButton.getUserData(), featureTemplatePicker.getSelectedFeatureTemplateItem());
+
+        secondButton.setSelected(false);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNull(featureTemplatePicker.getSelectedFeatureTemplateItem());
+
+        featureTemplatePicker.setSelectedFeatureTemplateItem((FeatureTemplateItem) firstButton.getUserData());
+        WaitForAsyncUtils.waitForFxEvents();
+        assertTrue(firstButton.isSelected());
+
+        featureTemplatePicker.setSelectedFeatureTemplateItem(null);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNull(firstButton.getToggleGroup().getSelectedToggle());
+
+        // focus
+        Platform.runLater(firstButton::requestFocus);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNull(featureTemplatePicker.getSelectedFeatureTemplateItem());
+
+        Platform.runLater(secondButton::requestFocus);
+        WaitForAsyncUtils.waitForFxEvents();
+        assertNull(featureTemplatePicker.getSelectedFeatureTemplateItem());
     }
 
     /**
@@ -166,33 +227,37 @@ public class TemplatePickerTest extends ApplicationTest {
      */
     @Test
     public void symbolSize() {
-        sleep(3000);
-
-        int height = 10;
-        int width = 500;
+        ArcGISFeatureTable featureTable = new ServiceFeatureTable(WILDFIRE_RESPONSE_URL);
+        FeatureLayer featureLayer = new FeatureLayer(featureTable);
 
         Platform.runLater(() -> {
-            templatePicker.symbolHeightProperty().set(height);
-            templatePicker.symbolWidthProperty().set(width);
+            FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayer);
+            stackPane.getChildren().add(featureTemplatePicker);
         });
+
+        sleep(3000);
+
+        Set<ImageView> imageViews = lookup(n -> n instanceof ImageView).queryAll();
+
+        FeatureTemplatePicker featureTemplatePicker = (FeatureTemplatePicker) stackPane.getChildren().get(0);
+        int prevSize = featureTemplatePicker.getSymbolSize();
+        int newSize = 100;
+        featureTemplatePicker.setSymbolSize(newSize);
 
         sleep(1000);
 
-        Set<ImageView> imageViews = lookup(n -> n instanceof ImageView).queryAll();
-        imageViews.forEach(imageView -> {
-            assertEquals(height, imageView.getImage().getHeight());
-            assertEquals(width, imageView.getImage().getWidth());
-        });
-    }
+        for (ImageView imageView : imageViews) {
+            assertEquals(newSize, imageView.getImage().getWidth());
+            assertEquals(newSize, imageView.getImage().getHeight());
+        }
 
-    @Test
-    public void overrideCss() {
-        scene.getStylesheets().add(getClass().getResource("/test.css").toExternalForm());
-        sleep(3000);
+        featureTemplatePicker.setSymbolSize(prevSize);
 
-        Set<TilePane> tilePanes = lookup(n -> n instanceof TilePane).queryAll();
-        clickOn(tilePanes.iterator().next().getChildren().get(0));
+        sleep(1000);
 
-        sleep(5000);
+        for (ImageView imageView : imageViews) {
+            assertEquals(prevSize, imageView.getImage().getWidth());
+            assertEquals(prevSize, imageView.getImage().getHeight());
+        }
     }
 }
