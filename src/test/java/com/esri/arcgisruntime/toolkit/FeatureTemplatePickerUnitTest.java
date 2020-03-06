f7d4048cf7200e2c7c091d78579633edc237b5fa
Tyler Schiewe <tyler.schiewe@gmail.com>
2020-03-06 15:30:25 -0800

    add unit tests

---
+++ src/test/java/com/esri/arcgisruntime/toolkit/FeatureTemplatePickerUnitTest.java
@@ -0,0 +1,115 @@
+package com.esri.arcgisruntime.toolkit;
+
+import com.esri.arcgisruntime.data.ServiceFeatureTable;
+import com.esri.arcgisruntime.layers.FeatureLayer;
+import javafx.application.Platform;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Automated integration tests for feature template picker.
+ */
+@DisplayName("feature template picker unit tests")
+public class FeatureTemplatePickerUnitTest {
+
+  private static final String WILDFIRE_RESPONSE_URL = "https://sampleserver6.arcgisonline" +
+      ".com/arcgis/rest/services/Wildfire/FeatureServer/0";
+
+  /**
+   * Starts the JavaFX platform before all tests.
+   */
+  @BeforeAll
+  private static void startPlatform() {
+    Platform.startup(() -> {});
+  }
+
+  /**
+   * Tests constructor with a null observable feature layers list.
+   */
+  @Test
+  @DisplayName("constructor with null observable feature layers list")
+  void nullObservableFeatureLayers() {
+    assertThrows(NullPointerException.class, () -> new FeatureTemplatePicker((ObservableList<FeatureLayer>) null));
+  }
+
+  /**
+   * Tests constructor with a null feature layer variable arg.
+   */
+  @Test
+  @DisplayName("constructor with null feature layer")
+  void nullFeatureLayer() {
+    assertThrows(NullPointerException.class, () -> new FeatureTemplatePicker((FeatureLayer) null));
+  }
+
+  /**
+   * Tests constructor with a feature layer array including null.
+   */
+  @Test
+  @DisplayName("constructor with null in feature layer array")
+  void nullFeatureLayers() {
+    assertThrows(NullPointerException.class, () -> new FeatureTemplatePicker(new FeatureLayer[]{null}));
+  }
+
+  @Test
+  @DisplayName("can get the feature layers")
+  void getFeatureLayers() {
+    FeatureLayer featureLayer = new FeatureLayer(new ServiceFeatureTable(WILDFIRE_RESPONSE_URL));
+    FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayer);
+    assertEquals(featureLayer, featureTemplatePicker.getFeatureLayers().get(0));
+  }
+
+  @Test
+  @DisplayName("layers backed by observable list")
+  void observableFeatureLayersList() {
+    ObservableList<FeatureLayer> featureLayers = FXCollections.observableArrayList();
+    FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayers);
+    assertEquals(featureLayers, featureTemplatePicker.getFeatureLayers());
+    assertEquals(0, featureTemplatePicker.getFeatureLayers().size());
+    FeatureLayer featureLayer = new FeatureLayer(new ServiceFeatureTable(WILDFIRE_RESPONSE_URL));
+    featureLayers.add(featureLayer);
+    assertEquals(1, featureTemplatePicker.getFeatureLayers().size());
+    assertEquals(featureLayer, featureTemplatePicker.getFeatureLayers().get(0));
+  }
+
+  @Test
+  @DisplayName("add layers via observable list")
+  void featureLayersObservableList() {
+    FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker();
+    FeatureLayer featureLayer = new FeatureLayer(new ServiceFeatureTable(WILDFIRE_RESPONSE_URL));
+    featureTemplatePicker.getFeatureLayers().add(featureLayer);
+    assertEquals(1, featureTemplatePicker.getFeatureLayers().size());
+    assertEquals(featureLayer, featureTemplatePicker.getFeatureLayers().get(0));
+  }
+
+  @Test
+  @DisplayName("bind feature layers to external property")
+  void bindFeatureLayersProperty() {
+    ListProperty<FeatureLayer> externalFeatureLayers = new SimpleListProperty<>(FXCollections.observableArrayList());
+    FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker();
+    featureTemplatePicker.featureLayersProperty().bind(externalFeatureLayers);
+    FeatureLayer featureLayer = new FeatureLayer(new ServiceFeatureTable(WILDFIRE_RESPONSE_URL));
+    externalFeatureLayers.add(featureLayer);
+    assertEquals(1, featureTemplatePicker.getFeatureLayers().size());
+    assertEquals(featureLayer, featureTemplatePicker.getFeatureLayers().get(0));
+  }
+
+  @Test
+  @DisplayName("can bind over initialized list")
+  void bindOverInitializeList() {
+    FeatureLayer featureLayer = new FeatureLayer(new ServiceFeatureTable(WILDFIRE_RESPONSE_URL));
+    ObservableList<FeatureLayer> featureLayers = FXCollections.observableArrayList(featureLayer);
+    FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker(featureLayers);
+    ListProperty<FeatureLayer> emptyListProperty = new SimpleListProperty<>(FXCollections.observableArrayList());
+    featureTemplatePicker.featureLayersProperty().bind(emptyListProperty);
+    assertEquals(0, featureTemplatePicker.getFeatureLayers().size());
+  }
+
+}
---
+++ src/test/java/com/esri/arcgisruntime/toolkit/skins/FeatureTemplatePickerTilePaneSkinUnitTest.java
@@ -0,0 +1,48 @@
+package com.esri.arcgisruntime.toolkit.skins;
+
+import com.esri.arcgisruntime.toolkit.FeatureTemplatePicker;
+import javafx.application.Platform;
+import javafx.geometry.Orientation;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("feature template picker tile pane skin unit tests")
+public class FeatureTemplatePickerTilePaneSkinUnitTest {
+
+  @BeforeAll
+  static void startPlatform() {
+    Platform.startup(() -> {});
+  }
+
+  @Test
+  @DisplayName("null control throws exception")
+  void nullControlConstructor() {
+    assertThrows(IllegalArgumentException.class, () -> new FeatureTemplatePickerTilePaneSkin(null));
+  }
+
+  @Test
+  @DisplayName("root child is scrollpane")
+  void scrollpaneChild() {
+    FeatureTemplatePickerTilePaneSkin skin = new FeatureTemplatePickerTilePaneSkin(new FeatureTemplatePicker());
+    assertEquals(1, skin.getChildren().size());
+    assertTrue(skin.getChildren().get(0) instanceof ScrollPane);
+  }
+  
+  @Test
+  @DisplayName("content node changes when orientation changes")
+  void contentNodeOrientation() {
+    FeatureTemplatePicker featureTemplatePicker = new FeatureTemplatePicker();
+    FeatureTemplatePickerTilePaneSkin skin = new FeatureTemplatePickerTilePaneSkin(featureTemplatePicker);
+    ScrollPane scrollPane = (ScrollPane) skin.getChildren().get(0);
+    featureTemplatePicker.setOrientation(Orientation.HORIZONTAL);
+    assertTrue(scrollPane.getContent() instanceof HBox);
+    featureTemplatePicker.setOrientation(Orientation.VERTICAL);
+    assertTrue(scrollPane.getContent() instanceof VBox);
+  }
+}
