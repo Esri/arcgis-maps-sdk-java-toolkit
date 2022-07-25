44d3199cde229dd805622c3598c488864fc67b4b
Jen Merritt <jmerritt@esri.com>
2022-07-25 15:18:52 +0100

    Configure settings of utility network example

--- arcgis-java-toolkit-examples/src/main/java/com/esri/arcgisruntime/toolkit/examples/UtilityNetworkTraceToolExample.java
+++ arcgis-java-toolkit-examples/src/main/java/com/esri/arcgisruntime/toolkit/examples/UtilityNetworkTraceToolExample.java
@@ -18,21 +18,24 @@ package com.esri.arcgisruntime.toolkit.examples;
 
 import com.esri.arcgisruntime.loadable.LoadStatus;
 import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.BasemapStyle;
 import com.esri.arcgisruntime.mapping.view.GeoView;
 import com.esri.arcgisruntime.mapping.view.MapView;
 import com.esri.arcgisruntime.portal.Portal;
 import com.esri.arcgisruntime.security.UserCredential;
+import com.esri.arcgisruntime.symbology.ColorUtil;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
 import com.esri.arcgisruntime.toolkit.UtilityNetworkTraceTool;
 import com.esri.arcgisruntime.toolkit.model.Example;
 import com.esri.arcgisruntime.toolkit.utils.ExampleUtils;
 import javafx.application.Application;
 import javafx.scene.Node;
-import javafx.scene.control.ProgressIndicator;
-import javafx.scene.control.Tab;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -136,6 +139,181 @@ public class UtilityNetworkTraceToolExample extends Application implements Examp
      */
     private VBox configureSettings() {
         ArrayList<Node> requiredSettings = new ArrayList<>();
+
+        // property settings
+        TitledPane propertyTitledPane = new TitledPane();
+        propertyTitledPane.setExpanded(false);
+        propertyTitledPane.setText("Properties");
+        VBox propertyVBox = new VBox(5);
+        propertyTitledPane.setContent(propertyVBox);
+        requiredSettings.add(propertyTitledPane);
+        // adding starting points
+        Label isAddingStartingPoints = new Label("Toggle isAddingStartingPoints:");
+        RadioButton addStartingPointT = new RadioButton("True");
+        addStartingPointT.setUserData(true);
+        RadioButton addStartingPointF = new RadioButton("False");
+        addStartingPointF.setUserData(false);
+        var addStartingPointToggleGroup = new ToggleGroup();
+        addStartingPointT.setToggleGroup(addStartingPointToggleGroup);
+        addStartingPointF.setToggleGroup(addStartingPointToggleGroup);
+        addStartingPointToggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.getUserData() instanceof Boolean) {
+                utilityNetworkTraceTool.isAddingStartingPointsProperty().set((Boolean) newValue.getUserData());
+            }
+        });
+        utilityNetworkTraceTool.isAddingStartingPointsProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                if (newValue) {
+                    addStartingPointToggleGroup.selectToggle(addStartingPointT);
+                } else {
+                    addStartingPointToggleGroup.selectToggle(addStartingPointF);
+                }
+            }
+        }));
+        if (utilityNetworkTraceTool.isAddingStartingPointsProperty().get()) {
+            addStartingPointToggleGroup.selectToggle(addStartingPointT);
+        } else {
+            addStartingPointToggleGroup.selectToggle(addStartingPointF);
+        }
+        propertyVBox.getChildren().addAll(isAddingStartingPoints, addStartingPointT, addStartingPointF);
+
+        // auto zoom to result
+        Label isAutoZoomToResults = new Label("Toggle isAutoZoomToResults");
+        RadioButton isAutoZoomT = new RadioButton("True");
+        isAutoZoomT.setUserData(true);
+        RadioButton isAutoZoomF = new RadioButton("False");
+        isAutoZoomF.setUserData(false);
+        var isAutoZoomToggleGroup = new ToggleGroup();
+        isAutoZoomT.setToggleGroup(isAutoZoomToggleGroup);
+        isAutoZoomF.setToggleGroup(isAutoZoomToggleGroup);
+        isAutoZoomToggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.getUserData() instanceof Boolean) {
+                utilityNetworkTraceTool.autoZoomToResultsProperty().set((Boolean) newValue.getUserData());
+            }
+        });
+        utilityNetworkTraceTool.autoZoomToResultsProperty().addListener(((observable, oldValue, newValue) -> {
+            if (newValue != null) {
+                if (newValue) {
+                    isAutoZoomToggleGroup.selectToggle(isAutoZoomT);
+                } else {
+                    isAutoZoomToggleGroup.selectToggle(isAutoZoomF);
+                }
+            }
+        }));
+        if (utilityNetworkTraceTool.autoZoomToResultsProperty().get()) {
+            isAutoZoomToggleGroup.selectToggle(isAutoZoomT);
+        } else {
+            isAutoZoomToggleGroup.selectToggle(isAutoZoomF);
+        }
+        propertyVBox.getChildren().addAll(isAutoZoomToResults, isAutoZoomT, isAutoZoomF);
+
+
+        // update starting point symbol color
+        Label updateStartingPointSymbolColor = new Label("Update starting point color");
+        ColorPicker colorPicker =
+                new ColorPicker(ColorUtil.argbToColor(
+                        ((SimpleMarkerSymbol) utilityNetworkTraceTool.getStartingPointSymbol()).getColor()));
+        colorPicker.setOnAction(event -> {
+            ((SimpleMarkerSymbol) utilityNetworkTraceTool.getStartingPointSymbol()).setColor(
+                    ColorUtil.colorToArgb(colorPicker.getValue()));
+        });
+        propertyVBox.getChildren().addAll(updateStartingPointSymbolColor, colorPicker);
+
+        // map settings
+        Label setNewMap = new Label("Set new ARCGISMap on MapView.");
+        TitledPane mapTitledPane = new TitledPane();
+        mapTitledPane.setExpanded(false);
+        mapTitledPane.setText("Map settings");
+        VBox mapVBox = new VBox(5);
+        mapTitledPane.setContent(mapVBox);
+        Label mapDescription = new Label("Also calls refresh() to reload data.");
+        ComboBox<ArcGISMap> mapComboBox = new ComboBox<>();
+        var map = new ArcGISMap(BasemapStyle.ARCGIS_IMAGERY);
+        mapComboBox.getItems().addAll(webMap, map);
+        mapComboBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(ArcGISMap arcGISMap) {
+                if (arcGISMap == webMap) {
+                    return "Webmap with utility networks";
+                } else {
+                    return "Map with no utility networks";
+                }
+            }
+            @Override
+            public ArcGISMap fromString(String string) {
+                return null;
+            }
+        });
+        mapComboBox.getSelectionModel().select(webMap);
+        mapComboBox.getSelectionModel().selectedItemProperty().addListener((obvs, ov, nv) -> {
+            if(nv.equals(webMap)) {
+                mapView.setMap(webMap);
+                utilityNetworkTraceTool.refresh();
+            } else {
+                mapView.setMap(map);
+                utilityNetworkTraceTool.refresh();
+            }
+        });
+        mapVBox.getChildren().addAll(setNewMap, mapDescription, mapComboBox);
+        requiredSettings.add(mapTitledPane);
+
+        // style settings
+        Label styleSettingsLabel = new Label("Show/Hide default styles.");
+        TitledPane stylesTitledPane = new TitledPane();
+        stylesTitledPane.setExpanded(false);
+        stylesTitledPane.setText("Syle settings");
+        VBox stylesVBox = new VBox(5);
+        stylesTitledPane.setContent(stylesVBox);
+        RadioButton showCustomStyles = new RadioButton("Show custom styles");
+        showCustomStyles.setUserData(true);
+        RadioButton hideCustomStyles = new RadioButton("Hide custom styles");
+        hideCustomStyles.setUserData(false);
+        var stylesToggleGroup = new ToggleGroup();
+        showCustomStyles.setToggleGroup(stylesToggleGroup);
+        hideCustomStyles.setToggleGroup(stylesToggleGroup);
+        showCustomStyles.setSelected(true);
+        stylesToggleGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.getUserData() instanceof Boolean) {
+                if ((Boolean) newValue.getUserData()) {
+                    primaryStage.getScene().getStylesheets().add("/styles/style.css");
+                } else {
+                    primaryStage.getScene().getStylesheets().remove("/styles/style.css");
+                }
+            }
+        });
+        stylesVBox.getChildren().addAll(styleSettingsLabel, showCustomStyles, hideCustomStyles);
+        requiredSettings.add(stylesTitledPane);
+
+        // Layout settings
+        TitledPane layoutTitledPane = new TitledPane();
+        layoutTitledPane.setExpanded(false);
+        layoutTitledPane.setText("Layout settings");
+        VBox layoutVBox = new VBox(5);
+        layoutTitledPane.setContent(layoutVBox);
+        Label sizeLabel = new Label("Resize:");
+        Slider sizeSlider = new Slider(120, 500, 220);
+        sizeSlider.setShowTickLabels(true);
+        sizeSlider.setMajorTickUnit(500);
+        sizeSlider.valueProperty().addListener((obvs, ov, nv) -> {
+            utilityNetworkTraceTool.setPrefSize(nv.doubleValue(), nv.doubleValue());
+        });
+        layoutVBox.getChildren().addAll(sizeLabel, sizeSlider);
+        // position
+        Label positionLabel = new Label("Re-position:");
+        ComboBox<String> positionComboBox = new ComboBox<>();
+        positionComboBox.getItems().addAll("Left", "Right");
+        positionComboBox.getSelectionModel().selectedItemProperty().addListener((obvs, ov, nv) -> {
+            if(nv.equals("Left")) {
+                mapViewBorderPane.setRight(null);
+                mapViewBorderPane.setLeft(utilityNetworkTraceTool);
+            } else {
+                mapViewBorderPane.setLeft(null);
+                mapViewBorderPane.setRight(utilityNetworkTraceTool);
+            }
+        });
+        positionComboBox.getSelectionModel().select("Right");
+        layoutVBox.getChildren().addAll(positionLabel, positionComboBox);
+        requiredSettings.add(layoutTitledPane);
 
         return ExampleUtils.createSettings(getName(), requiredSettings);
     }
