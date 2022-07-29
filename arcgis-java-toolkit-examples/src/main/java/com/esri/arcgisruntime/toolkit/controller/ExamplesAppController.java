8ab45924c40b2c2574dfaca092daa04f8eba0bcb
Jen Merritt <jmerritt@esri.com>
2022-07-29 11:50:53 +0100

    fix spacing and imports across example app

--- arcgis-java-toolkit-examples/src/main/java/com/esri/arcgisruntime/toolkit/controller/ExamplesAppController.java
+++ arcgis-java-toolkit-examples/src/main/java/com/esri/arcgisruntime/toolkit/controller/ExamplesAppController.java
@@ -16,6 +16,9 @@
 
 package com.esri.arcgisruntime.toolkit.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
 import com.esri.arcgisruntime.mapping.view.GeoView;
 import com.esri.arcgisruntime.toolkit.examples.CompassExample;
@@ -34,9 +37,6 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.util.StringConverter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * A Controller for the {@link com.esri.arcgisruntime.toolkit.ExamplesApp}. This includes a header section with ComboBox
  * menu to select an Example to view, and also includes an initial Landing Page that displays when the app first loads
@@ -46,139 +46,139 @@ import java.util.List;
  */
 public class ExamplesAppController {
 
-    private final List<Example> examples = new ArrayList<>();
-    @FXML private ComboBox<Example> menu;
-    @FXML private ExampleView exampleView;
-    @FXML private GridPane examplesGridPane;
-    @FXML private VBox landingPage;
+  private final List<Example> examples = new ArrayList<>();
+  @FXML private ComboBox<Example> menu;
+  @FXML private ExampleView exampleView;
+  @FXML private GridPane examplesGridPane;
+  @FXML private VBox landingPage;
 
-    /**
-     * Sets up and populates the UI.
-     *
-     * @since 100.15.0
-     */
-    public void initialize() {
-        // authentication with an API key or named user is required to access basemaps and other location services
-        ArcGISRuntimeEnvironment.setApiKey(System.getProperty("apiKey"));
+  /**
+   * Sets up and populates the UI.
+   *
+   * @since 100.15.0
+   */
+  public void initialize() {
+    // authentication with an API key or named user is required to access basemaps and other location services
+    ArcGISRuntimeEnvironment.setApiKey(System.getProperty("apiKey"));
 
-        // adds all the examples to the UI
-        examples.addAll(getExamples());
-        examples.forEach(example -> menu.getItems().add(example));
-        // displays the examples by name in the menu
-        menu.setConverter(new StringConverter<>() {
-            @Override
-            public String toString(Example example) {
-                if (example == null) {
-                    return null;
-                } else {
-                    return example.getName();
-                }
-            }
-            @Override
-            public Example fromString(String string) {
-                return null;
-            }
-        });
-        // displays a checkmark in the menu on the selected item via CSS
-        menu.setCellFactory(lv -> {
-            final ListCell<Example> cell = new ListCell<>() {
-                @Override
-                public void updateItem(Example item, boolean empty) {
-                    super.updateItem(item, empty);
-                    setText(item != null ? item.getName() : null);
-                }
-            };
-            Region icon = new Region();
-            icon.getStyleClass().add("icon");
-            cell.setGraphic(icon);
-            cell.setGraphicTextGap(20);
-            return cell;
-        });
-
-        // sets the selected example to the view
-        menu.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            if (landingPage.isVisible()) {
-                // hide the landing page once a selection is made
-                landingPage.setVisible(false);
-            }
-            exampleView.setSelectedExample(newValue);
-        });
-        // toggles the visibility of the landing page and example view
-        exampleView.visibleProperty().bind(landingPage.visibleProperty().not());
-        // configures the grid pane of examples on the landing page
-        setupExampleGridPane();
-    }
-
-    /**
-     * Returns a list of Examples.
-     *
-     * @return the list of examples
-     * @since 100.15.0
-     */
-    private List<Example> getExamples() {
-        Example compassExample = new CompassExample();
-        Example floorFilterExample = new FloorFilterExample();
-        return List.of(compassExample, floorFilterExample);
-    }
-
-    /**
-     * Setups up the GridPane of examples displayed on the landing page. This is a 2 column grid with as many rows
-     * as is required for the number of examples. The GridPane is setup in app.fxml.
-     *
-     * @since 100.15.0
-     */
-    private void setupExampleGridPane() {
-        var numberOfColumns = 2;
-        var indexOfExample = 0;
-        // add each of the examples into the grid at the appropriate column and row
-        while (indexOfExample < examples.size()) {
-            int row = indexOfExample / numberOfColumns;
-            int col = indexOfExample % numberOfColumns;
-            var example = examples.get(indexOfExample);
-            // HBox container for each example
-            HBox hbox = new HBox(15);
-            hbox.getStyleClass().add("panel");
-            hbox.getStyleClass().add("panel-white");
-            hbox.setId("example-grid-pane-item");
-            hbox.setAlignment(Pos.CENTER_LEFT);
-            // on clicking the HBox the example will be selected
-            hbox.setOnMouseClicked(e -> menu.getSelectionModel().select(example));
-            // ImageView displays thumbnail of the component
-            ImageView imageView = new ImageView();
-            imageView.setFitWidth(100);
-            imageView.setFitHeight(100);
-            // check if an image exists for the example and display the default if not
-            if (ExamplesAppController.class.getResource("/images/" + example.getName() + ".png") != null) {
-                imageView.setImage(new Image("/images/" + example.getName() + ".png"));
-            } else if (ExamplesAppController.class.getResource("/images/default.png") != null){
-                imageView.setImage(new Image("/images/default.png"));
-            }
-            // VBox containing the name and description for the example
-            var labelVBox = new VBox(8);
-            labelVBox.getStyleClass().add("panel-no-padding, panel-no-border, panel-white");
-            labelVBox.setAlignment(Pos.CENTER_LEFT);
-            var componentName = new Label(example.getName());
-            componentName.getStyleClass().add("h2");
-            componentName.getStyleClass().add("blue-text");
-            componentName.getStyleClass().add("label-wrap-text");
-            var componentDescription = new Label(example.getDescription());
-            componentDescription.getStyleClass().add("label-wrap-text");
-            labelVBox.getChildren().addAll(componentName, componentDescription);
-            // add child components to the HBox
-            hbox.getChildren().addAll(imageView, labelVBox);
-            // add the example to the GridPane
-            examplesGridPane.add(hbox, col, row);
-            // increment the index to loop through the next example
-            indexOfExample += 1;
+    // adds all the examples to the UI
+    examples.addAll(getExamples());
+    examples.forEach(example -> menu.getItems().add(example));
+    // displays the examples by name in the menu
+    menu.setConverter(new StringConverter<>() {
+      @Override
+      public String toString(Example example) {
+        if (example == null) {
+          return null;
+        } else {
+          return example.getName();
         }
-    }
+      }
+      @Override
+      public Example fromString(String string) {
+        return null;
+      }
+    });
+    // displays a checkmark in the menu on the selected item via CSS
+    menu.setCellFactory(lv -> {
+      final ListCell<Example> cell = new ListCell<>() {
+        @Override
+        public void updateItem(Example item, boolean empty) {
+          super.updateItem(item, empty);
+          setText(item != null ? item.getName() : null);
+        }
+      };
+      Region icon = new Region();
+      icon.getStyleClass().add("icon");
+      cell.setGraphic(icon);
+      cell.setGraphicTextGap(20);
+      return cell;
+    });
 
-    /**
-     * Disposes the views for each example on termination.
-     *
-     * @since 100.15.0
-     */
-    public void terminate() {
-        examples.forEach(example -> example.getGeoViews().forEach(GeoView::dispose));
+    // sets the selected example to the view
+    menu.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+      if (landingPage.isVisible()) {
+        // hide the landing page once a selection is made
+        landingPage.setVisible(false);
+      }
+      exampleView.setSelectedExample(newValue);
+    });
+    // toggles the visibility of the landing page and example view
+    exampleView.visibleProperty().bind(landingPage.visibleProperty().not());
+    // configures the grid pane of examples on the landing page
+    setupExampleGridPane();
+  }
+
+  /**
+   * Returns a list of Examples.
+   *
+   * @return the list of examples
+   * @since 100.15.0
+   */
+  private List<Example> getExamples() {
+    Example compassExample = new CompassExample();
+    Example floorFilterExample = new FloorFilterExample();
+    return List.of(compassExample, floorFilterExample);
+  }
+
+  /**
+   * Setups up the GridPane of examples displayed on the landing page. This is a 2 column grid with as many rows
+   * as is required for the number of examples. The GridPane is setup in app.fxml.
+   *
+   * @since 100.15.0
+   */
+  private void setupExampleGridPane() {
+    var numberOfColumns = 2;
+    var indexOfExample = 0;
+    // add each of the examples into the grid at the appropriate column and row
+    while (indexOfExample < examples.size()) {
+      int row = indexOfExample / numberOfColumns;
+      int col = indexOfExample % numberOfColumns;
+      var example = examples.get(indexOfExample);
+      // HBox container for each example
+      HBox hbox = new HBox(15);
+      hbox.getStyleClass().add("panel");
+      hbox.getStyleClass().add("panel-white");
+      hbox.setId("example-grid-pane-item");
+      hbox.setAlignment(Pos.CENTER_LEFT);
+      // on clicking the HBox the example will be selected
+      hbox.setOnMouseClicked(e -> menu.getSelectionModel().select(example));
+      // ImageView displays thumbnail of the component
+      ImageView imageView = new ImageView();
+      imageView.setFitWidth(100);
+      imageView.setFitHeight(100);
+      // check if an image exists for the example and display the default if not
+      if (ExamplesAppController.class.getResource("/images/" + example.getName() + ".png") != null) {
+        imageView.setImage(new Image("/images/" + example.getName() + ".png"));
+      } else if (ExamplesAppController.class.getResource("/images/default.png") != null){
+        imageView.setImage(new Image("/images/default.png"));
+      }
+      // VBox containing the name and description for the example
+      var labelVBox = new VBox(8);
+      labelVBox.getStyleClass().add("panel-no-padding, panel-no-border, panel-white");
+      labelVBox.setAlignment(Pos.CENTER_LEFT);
+      var componentName = new Label(example.getName());
+      componentName.getStyleClass().add("h2");
+      componentName.getStyleClass().add("blue-text");
+      componentName.getStyleClass().add("label-wrap-text");
+      var componentDescription = new Label(example.getDescription());
+      componentDescription.getStyleClass().add("label-wrap-text");
+      labelVBox.getChildren().addAll(componentName, componentDescription);
+      // add child components to the HBox
+      hbox.getChildren().addAll(imageView, labelVBox);
+      // add the example to the GridPane
+      examplesGridPane.add(hbox, col, row);
+      // increment the index to loop through the next example
+      indexOfExample += 1;
     }
+  }
+
+  /**
+   * Disposes the views for each example on termination.
+   *
+   * @since 100.15.0
+   */
+  public void terminate() {
+    examples.forEach(example -> example.getGeoViews().forEach(GeoView::dispose));
+  }
 }
