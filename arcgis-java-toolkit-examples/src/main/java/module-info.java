e441f593c6e4d48ec57e9183c300e7233866058a
Jen Merritt <jmerritt@esri.com>
2022-12-05 12:42:12 +0000

    Toolkit module (#65)
    * delete launchers
    
    * refactor file and resource folder structure
    
    * update resource refs
    
    * toolkit: add module-info and make required updates
    
    * examples: add module-info and make required updates
    
    * toolkit: require default version
    
    * updates to module declarations in example app
    
    * update daily build no
    
    * updates to module declarations in toolkit
    
    * exclude javafx dependencies from pom
    
    * do not generate module file
    
    * rebranding updates in module file
    
    * fix default version to number not string
    
    * update readme
    
    * tidy folder structure

---
+++ arcgis-java-toolkit-examples/src/main/java/module-info.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+module com.esri.arcgisruntime.toolkit.examples {
+  // require ArcGIS Maps SDK for Java module
+  requires com.esri.arcgisruntime;
+  // require ArcGIS Maps SDK for Java toolkit module
+  requires com.esri.arcgisruntime.toolkit;
+  // require JavaFX modules the application uses
+  requires javafx.graphics;
+  requires javafx.controls;
+  requires javafx.fxml;
+
+  // require other modules the application uses
+  requires java.logging;
+
+  exports com.esri.arcgisruntime.toolkit.examples;
+  exports com.esri.arcgisruntime.toolkit.examples.controller;
+  exports com.esri.arcgisruntime.toolkit.examples.utils;
+  // allow FXML module access to Example FXML files
+  opens com.esri.arcgisruntime.toolkit.examples to javafx.fxml;
+  opens com.esri.arcgisruntime.toolkit.examples.controller to javafx.fxml;
+}
